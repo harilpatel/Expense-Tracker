1d52539343202d5426bca3abe96651fd286d027f
Haril Patel  m-5-35-shastrinagar <harilpatel1512@gmail.com>
2022-04-13 11:40:26 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Balance from './component/Balance';
+import Header from './component/Header';
+import Income from './component/Income';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+      <div>
+        <Income />
+      </div>
     </div>
   );
 }
